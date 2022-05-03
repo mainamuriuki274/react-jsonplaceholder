9af486637fe42711b945c4283b61f635f602aaa2
Lewis Maina Muriuki <43462574+mainamuriuki274@users.noreply.github.com>
2022-05-03 03:52:06 +0300

    (feat): display the number of users living in an Appt, and in a Suite (#9)
    * add number of users living in apartments and suites

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,23 +1,64 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
+import { Error } from '../../components/Error';
 import { HomeCard } from '../../components/HomeCard';
+import { Loader } from '../../components/Loader';
+import { useFetchMany } from '../../hooks/useFetchMany';
 import styles from './styles.module.scss';
 
-const HomePage: FunctionComponent = () => (
-  <div className={styles.home_container}>
-    <h1>Welcome to a Simple React JSONPlaceholder Project</h1>
-    <div className={styles.cards_container}>
-      <HomeCard
-        link="/users"
-        title="Users"
-        description="Displays a list of all users"
-      />
-      <HomeCard
-        link="/posts"
-        title="Posts"
-        description="Displays all posts and its details"
-      />
-    </div>
-  </div>
-);
+interface Address {
+  suite: string;
+}
 
+interface User {
+  id: number;
+  address: Address;
+}
+
+const HomePage: FunctionComponent = () => {
+  const [apptNumber, setApptNumber] = useState<number>(0);
+  const [suiteNumber, setSuiteNumber] = useState<number>(0);
+  const { data: users, isPending, error } = useFetchMany<User>('/users');
+
+  useEffect(() => {
+    if (users) {
+      setApptNumber(
+        users.filter((usr) => usr.address.suite.includes('Apt')).length,
+      );
+      setSuiteNumber(
+        users.filter((usr) => usr.address.suite.includes('Suite')).length,
+      );
+    }
+  }, [users]);
+
+  if (isPending) return <Loader />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className={styles.home_container}>
+      <h1>Welcome to a Simple React JSONPlaceholder Project</h1>
+      <div className="numbers">
+        <p>
+          There are &nbsp;
+          {suiteNumber}
+          &nbsp; users living in Suites and &nbsp;
+          {apptNumber}
+          &nbsp;in Apartments.
+        </p>
+      </div>
+      <div className={styles.cards_container}>
+        <HomeCard
+          link="/users"
+          title="Users"
+          description="Displays a list of all users"
+        />
+        <HomeCard
+          link="/posts"
+          title="Posts"
+          description="Displays all posts and its details"
+        />
+      </div>
+    </div>
+  );
+};
 export default HomePage;
